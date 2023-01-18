03eca99974301f2ea84ed1e60e1c75013c661c87
Everly <66821749+everlyy@users.noreply.github.com>
2023-01-18 19:25:29 +0100

    Remove cat and update kernel

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,14 +1,16 @@
 #include <types.h>
-#include <vbe/vbe.h>
-#include "cat_image.h"
-
-#define ASM __asm__ __volatile__
+#include <serial/serial.h>
+#include <serial/debug.h>
 
 __attribute__((section("kernel_entry"))) void kernel_main(void) {
-	for(uint32_t y = 0; y < image_h; y++) {
-		for(uint32_t x = 0; x < image_w; x++) {
-			vbe_set_pixel(x, y, image[y * image_w + x]);
-		}
-	}
+	serial_init();
+
+	debug_print_string("Hello, serial!\n");
+	debug_print_dec(12345);
+	debug_print_char('\n');
+	debug_print_hex(0x1234ABCD);
+	debug_print_char('\n');
+
+
 	while(1);
 }
